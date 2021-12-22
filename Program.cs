e9fc8925d4f6e3811db7a97d0954aad31bdfa7d1
Daniël Knippers <dknippers@gmail.com>
2021-12-22 20:55:36 +0100

    Target updates

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sandbox
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
